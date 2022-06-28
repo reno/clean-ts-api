f18f869758546d5ede9545ec0dc74611c33f65aa
Renan Modenese <renan.modenese@gmail.com>
2022-06-28 10:58:15 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controlllers/singup.spec.ts
+++ src/presentation/controlllers/singup.spec.ts
@@ -17,4 +17,17 @@ describe('Signup controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   });
 
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any name',
+        password: 'anypassword',
+        passwordConfirmation: 'anypassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  });
 });
