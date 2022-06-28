9101e874532321fb77e2c65b97a0e81aef5875e7
Renan Modenese <renan.modenese@gmail.com>
2022-06-28 15:19:02 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controlllers/signup/singup.spec.ts
+++ src/presentation/controlllers/signup/singup.spec.ts
@@ -202,4 +202,24 @@ describe('Signup controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   });
+
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid name',
+        email: 'valid@mail.com',
+        password: 'validpassword',
+        passwordConfirmation: 'validpassword'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid name',
+      email: 'valid@mail.com',
+      password: 'validpassword',
+    })
+  })
 });
