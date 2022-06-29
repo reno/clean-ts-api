85aec1607b4aced2b83f5a76bf61ed2524d5e6b9
Renan Modenese <renan.modenese@gmail.com>
2022-06-29 10:52:31 -0300

    feat: ensure DbAddAccount calls Encrypter with correct passoword

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -7,9 +7,5 @@ export interface AddAccountModel {
 }
 
 export interface AddAccount {
-
-  add(account: AddAccountModel): Promise<AccountModel> {
-
-}
-
+  add(account: AddAccountModel): Promise<AccountModel>
 }
