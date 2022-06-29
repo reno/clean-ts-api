f97b2c42592c1b9da2668b471c81ac49f354fa90
Renan Modenese <renan.modenese@gmail.com>
2022-06-29 09:40:53 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,11 @@
+import { EmailValidatorAdapter } from './email-validator';
+
+describe('EmailValidatorAdapter', () => {
+
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('invalid@mail.com')
+    expect(isValid).toBe(false)
+  })
+
+})
