ca7fcd929d8f69a8ac96461426b62048b2c23802
Renan Modenese <renan.modenese@gmail.com>
2022-07-01 10:44:52 -0300

    refactor: change helper method name

--- src/presentation/controlllers/login/login.spec.ts
+++ src/presentation/controlllers/login/login.spec.ts
@@ -4,7 +4,7 @@ import { InvalidParamError, MissingParamError } from '../../errors';
 import { HttpRequest } from '../../protocols';
 import { EmailValidator } from '../signup/signup-protocols';
 
-const makeHttpRequest = (): HttpRequest => (
+const makeFakeRequest = (): HttpRequest => (
   {
     body: {
       email: 'any@mail.com',
@@ -65,7 +65,7 @@ describe('Login Controller', () => {
   test('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut()
     const spy = jest.spyOn(emailValidatorStub, 'isValid')
-    const httpRequest = makeHttpRequest()
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(spy).toHaveBeenCalledWith(httpRequest.body.email)
   })
