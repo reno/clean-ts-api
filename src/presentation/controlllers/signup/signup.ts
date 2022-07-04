4e65fe5c9df385495b827ef4e4f6013b2af3d35b
Renan Modenese <renan.modenese@gmail.com>
2022-07-04 10:19:43 -0300

    refactor: remove references to EmailValidator

--- src/presentation/controlllers/signup/signup.ts
+++ src/presentation/controlllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, Validation } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, AddAccount, Validation } from './signup-protocols'
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
