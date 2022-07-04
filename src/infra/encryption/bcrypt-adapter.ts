b6a1cabe07345ccbc3afbeeb8c2ccf89ba264bec
Renan Modenese <renan.modenese@gmail.com>
2022-07-04 15:42:59 -0300

    feat: ensure BCryptAdapter calls bcrypt compare with correct values

--- src/infra/encryption/bcrypt-adapter.ts
+++ src/infra/encryption/bcrypt-adapter.ts
@@ -11,4 +11,8 @@ export class BcryptAdapter implements Encrypter {
   async encrypt(value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
   }
+
+  async compare(value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
+  }
 }
